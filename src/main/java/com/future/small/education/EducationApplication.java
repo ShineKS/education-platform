ccdefd823acf66dd387612347fdbb48b759affbc
huangqi <huang.qi@vpclub.cn>
2018-10-14 17:23:47 +0800

    项目框架初始化

---
+++ src/main/java/com/future/small/education/EducationApplication.java
@@ -0,0 +1,12 @@
+package com.future.small.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationApplication.class, args);
+	}
+}
