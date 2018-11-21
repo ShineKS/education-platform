f8de54b7b92a1233cf45437b2a6ecd1902f550d2
huangqi <huang.qi@vpclub.cn>
2018-11-21 21:16:35 +0800

    项目结构初始化

---
+++ app-web/src/main/java/com/little/edu/appweb/AppWebApplication.java
@@ -0,0 +1,12 @@
+package com.little.edu.appweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppWebApplication.class, args);
+    }
+}
